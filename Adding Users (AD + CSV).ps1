78afad00d3e3575eea1f59358d68d39f5108bcb7
G D U L A <142990595+dzidulajubilee@users.noreply.github.com>
2024-10-01 02:43:27 -0700

    Add files via upload
    Adding Users With Powershell and CSV file.

---
+++ Adding Users (AD + CSV).ps1
@@ -0,0 +1,35 @@
+﻿#Remember to Change the Organization Unit in your CSV file Before Running this Script
+
+
+
+#import required modules
+Import-Module ActiveDirectory
+
+
+#Prompt User for CSV file path
+$filepath = Read-Host "Please Enter the path to your CSV file"
+
+#import the file into a variable
+$users = Import-Csv $filepath
+
+ForEach ($user in $users) {
+
+#Gather the User's information
+
+$fname= $user.Firstname
+$lname= $user.Lastname
+$ounit= $user.'Organizational Unit'
+$password= $user.Password
+$uname = $user.Username  
+$ophone=$user.Phone
+$email=$user.Email
+$dname=$user.Dname
+$securePassword =  ConvertTo-SecureString $user.password -AsPlainText -Force
+
+
+New-ADuser -Name "$fname $lname" -GivenName $fname -Surname $lname -SamAccountName $uname -DisplayName "$dname" -UserPrincipalName "$fname.$lname" -Path $ounit -AccountPassword $securePassword -ChangePasswordAtLogon $false -OfficePhone $ophone -Description $descript -Enabled $True -EmailAddress $email
+
+echo "Account Created for $fname $lname in $ounit"
+
+}
+
